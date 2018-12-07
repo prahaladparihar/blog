ae95f90d80b6d0e5449c187eae35b999ae7cdca0
Pariharpp <pariharprahalad26@gmail.com>
2018-12-08 00:58:09 +0530

    update

---
+++ src/components/ShowBlog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="container my-3 col-lg-8">
+      <h3>OUTPUT</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowBlog"
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ Vue.use(VueAxios, axios)
 
 import Headr from './components/Headr.vue'
 import Boody from './components/Boody.vue'
+import ShowBlog from './components/ShowBlog.vue'
 import Foottr from './components/Foottr.vue'
 
 
 Vue.component('headr', Headr);
 Vue.component('boody', Boody);
+Vue.component('showblog', ShowBlog);
 Vue.component('foottr', Foottr);
 new Vue({
     el: '#app',
